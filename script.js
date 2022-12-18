302e1c4466c94ee9ac267145c04d4f6206f05521
Yasmin Menezes <yasminmsmenezes@gmail.com>
2022-12-18 00:07:36 -0300

    changing style

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const baseURLImage = "https://image.tmdb.org/t/p/original/"
 const apiKey = "b14ca70e6b5beaaacfd1880619a13b29"
 const containerPoster = document.querySelector('.containerPoster')
 
-async function getMovies(){
+async function getAllMovies(){
     const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`, {
         headers: {
             Authorization: "b14ca70e6b5beaaacfd1880619a13b29"
@@ -10,7 +10,7 @@ async function getMovies(){
     })
     const data = await response.json();
     const result = data.results;
-    
+
     for(i=0; i<result.length; i++){    
             const image = document.createElement('img')
             image.style.cursor = 'pointer'
@@ -20,4 +20,32 @@ async function getMovies(){
 
 }
 
-getMovies()
+getAllMovies()
+
+// async function getAllSeries(){
+//     const response = await fetch(`
+//     https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US&page=1`, {
+//         headers: {
+//             Authorization: "b14ca70e6b5beaaacfd1880619a13b29"
+//         }
+//     })
+
+//     const data = await response.json();
+//     const result = data.results;
+//     console.log(data)
+//     console.log(result)
+
+//     for(i=0; i<result.length; i++){    
+//             const image = document.createElement('img')
+//             image.style.cursor = 'pointer'
+//             image.setAttribute('src', `${baseURLImage}${result[i].poster_path}`)
+//             image.addEventListener('click', () =>{
+//                 window.location.href = `./tvshow.html`
+//             })
+//             containerPoster.appendChild(image)
+//     }
+
+// }
+
+
+// getMovieByID https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
